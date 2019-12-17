5417c7928ace0fba4ac171fb677b4e25ada657bc
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-16 21:38:40 -0700

    Module 7: Creating a Promise

--- webpack-starter/src/scripts/index.js
+++ webpack-starter/src/scripts/index.js
@@ -1,11 +1,9 @@
 
-try{
-    //code
-    throw new Error('my custom error');
-}
-catch(error){
-    console.log('error: ', error);
-}
-finally{
-    console.log('this always executes');
-}
+
+let promise = new Promise(
+    function(resolve,reject){
+        setTimeout(reject,100,'someValue');
+    }
+);
+
+console.log(promise);
